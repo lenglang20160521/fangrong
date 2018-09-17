0821852b46cb7530608e4bf1249a66d1311356fc
sunrongge <1067095040@qq.com>
2018-09-17 10:32:28 +0800

    隐藏密码

--- src/main/java/config/YuanJingConfig.java
+++ src/main/java/config/YuanJingConfig.java
@@ -2,11 +2,11 @@ package config;
 
 public class YuanJingConfig {
     //broker 地址,tcp协议端口统一为11883
-    public  static  final  String brokerHost = "tcp://dev.envisioncn.com:11883";
+    public  static  final  String brokerHost = "***************************8";
     //设备名称
     public  static  final String userName = "connect1";
     //设备秘钥
-    public  static  final String password = "hSxoKXgUUWe0n97gjTub+teaih5h78mvNHnDNLOAT46VGKrpjNQmG9AuOko=";
+    public  static  final String password = "*************************";
     //topicName topic名称，该topic名称与Thing所用的策略绑定，需要保证策略拥有对应topic的订阅或者发送权限
     public  static  final String topic = "Master";
     //连接client名称，可以用户自定义，只需要保证用户所用的名称不重复即可
